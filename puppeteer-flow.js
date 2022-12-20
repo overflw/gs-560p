120e4ba144e42996eb1ec8c593b2cdd55d27938d
flow <git@lowspace.de>
2022-12-20 11:40:22 +0100

    Wip

--- puppeteer-flow.js
+++ puppeteer-flow.js
@@ -1,6 +1,9 @@
 const puppeteer = require("puppeteer");
 const microtime = require("microtime");
 
+
+let sleep = ms => new Promise(r => setTimeout(r, ms));
+
 (async () => {
   console.log(microtime.now(), " Launching Browser");
   const browser = await puppeteer.launch({
@@ -20,13 +23,64 @@ const microtime = require("microtime");
       deviceScaleFactor: window.devicePixelRatio,
     };
   });
-  // console.log("Dimensions:", dimensions); // Only to double-check if the dimensions are as expected
+  console.log("Dimensions:", dimensions); // Only to double-check if the dimensions are as expected
 
-  console.log(microtime.now(), " Home Page");
-  await page.goto("http://youtube.com", {
+  console.log(microtime.now(), " Open Youtube");
+  await page.goto("https://www.youtube.com/embed/aqz-KE-bpKQ", {
     waitUntil: "networkidle2",
   });
+  await sleep(500);
 
-  console.log(microtime.now(), " Closing Browser");
+  // Start youtube video and set quality
+
+  await page.evaluate(async () => {
+    let sleep = ms => new Promise(r => setTimeout(r, ms));
+    const quality = '480p';
+
+    // Enable quality settingsbutton
+    let playButton = document.getElementsByClassName("ytp-large-play-button")[0];
+    playButton.click();
+    await sleep(500);
+
+    let settingsButton = document.getElementsByClassName("ytp-settings-button")[0];
+    settingsButton.click();
+    await sleep(500);
+
+    let qualityMenu = document.getElementsByClassName("ytp-panel-menu")[0].lastChild;
+    qualityMenu.click();
+    await sleep(500);
+
+    let qualityOptions = [...document.getElementsByClassName("ytp-menuitem")];
+    let selection;
+    if (quality == 'Highest') selection = qualityOptions[0];
+    else selection = qualityOptions.filter(el => el.innerText == quality)[0];
+
+    if (!selection) {
+      let qualityTexts = qualityOptions.map(el => el.innerText).join('\n');
+      console.log('"' + quality + '" not found. Options are: \n\nHighest\n' + qualityTexts);
+      settingsButton.click();                               // click menu button to close
+      return;
+    }
+
+    if (selection.attributes['aria-checked'] === undefined) { // not checked
+      selection.click();
+      console.log('Quality set to ' + selection.textContent);
+    } else settingsButton.click();                            // click menu button to close
+
+    // start the video
+    let video = document.querySelector('video');
+    video.play();
+  });
+
+  console.log(microtime.now(), " Started videoplayback (1 minute)");
+  /*await page.screenshot({
+    path: 'screenshot1.jpg'
+  });*/
+  await sleep(1000 * 10);
+  /*await page.screenshot({
+    path: 'screenshot10.jpg'
+  });*/
+
+  console.log(microtime.now(), " Finished playback and closing browser");
   await browser.close();
 })();
